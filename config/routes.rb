12cc296ec6fa5bac1e149c9558e0680b83bf860f
Robin Weidlich <robinweidlich7@gmail.com>
2020-04-14 12:20:53 -0300

    adsfadsfadsf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	get 'restaurants/index'
+	get 'restaurants/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
